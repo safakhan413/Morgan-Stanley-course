7a3ef1920f21e3a28b4298a4eeaea3a727d3f065
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2021-11-17 03:23:38 -0400

    Update Coffee Shop.py

--- Coffee Shop.py
+++ Coffee Shop.py
@@ -1,32 +1,41 @@
 size = ""
 type = ""
+flavor = ""
 state = True
 
 def comparisons(variable, options):
     st = True
     for i in options:
-        # print(i)
         if variable == i:
             return st
     return False
 
 
 while state:
-    size = input("Choose coffee size: 'Small (S), Medium (M), Large(L):'")
+    size = input("Choose coffee size: 'Small (S), Medium (M), Large(L):'").upper()
     x = comparisons(size, ['S','M','L'])
     if x:
         state= False
-    # print('hey')
-    # print(x)
-    # if size == 'S' or size == 'M' or size == 'L':
-    #     state = "False"
-        # type = input("Type of coffee: 'brewed (S), espresso (E), cold brew(C):'")
-        # # if type == 'E' or type == 'S' or type == 'C':
-        # #     print(type)
-        # #  else:
-        # #     print("Please input a valid size")
-        # #     continue
-        # print(size)
     else:
         print("Please input a valid size")
         continue
+state = True
+while state:
+    type = input("Type of coffee: 'brewed (S), espresso (E), cold brew(C):'").upper()
+    x = comparisons(type, ['E','S','C'])
+    if x:
+        state= False
+    else:
+        print("Please input a valid type")
+        continue
+state = True
+while state:
+    flavor = input("Type of flavoring: 'None (N), All other options(A):'").upper()
+    x = comparisons(flavor, ['N','A'])
+    if x:
+        state= False
+    else:
+        print("Please input a valid type")
+        continue
+
+print(size,type,flavor)
